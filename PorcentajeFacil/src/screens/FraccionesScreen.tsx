bd1c114a160d6caf683310d43b954eb2eadf7dc7
AntonioSilvan <jjoosseessiillvvaann@gmail.com>
2021-12-08 23:55:54 -0600

    Finalizando funcionalidad de las vistas

--- PorcentajeFacil/src/screens/FraccionesScreen.tsx
+++ PorcentajeFacil/src/screens/FraccionesScreen.tsx
@@ -1,10 +1,63 @@
-import React from "react";
-import { Text,View } from 'react-native';
+import React, { useState } from "react";
+import { Text,View, TextInput, Keyboard } from 'react-native';
+import { styles } from "../theme/AppTheme";
+import { ActionsButtonsComponent } from "../components/ActionButtons/ActionButtons.component";
 
 export const FraccionesScreen = () => {
+
+    const [result, setResult] = useState('0.0');
+    const [form, setForm] = useState({
+        numerador: '',
+        denominador: ''
+    });
+
+    const onChanges = (value:string, field:string) => {
+        setForm({
+            ...form,
+            [field]: value
+        });
+    }
+
+    const calcular = () => {
+        const numerador = parseInt(form.numerador);
+        const denominador = parseInt(form.denominador);
+
+        const resultado = (numerador / denominador)*100;
+        setResult(resultado.toString());
+        Keyboard.dismiss();
+    }
+
+    const limpiar = () => {
+        setForm({
+            numerador: '',
+            denominador: ''
+        });
+        setResult('0.0');
+    }
+
     return (
         <View>
-            <Text>Fracciones</Text>
+            <View style={styles.calculationPanel}>
+                <View style={styles.simpleEntries}>
+                    <View style={{flex: 2}}>
+                        <Text style={styles.labelNumberInput}>Fraccion </Text>
+                    </View>
+                    <View style={{flex: 2}}>
+                        <TextInput style={styles.numberInput} placeholder="0.0" keyboardType="numeric" value={form.numerador} onChangeText={ (value) => onChanges(value, 'numerador')}/>
+                    </View>
+                    <View style={{flex: 1}}>
+                        <Text style={styles.labelNumberInput}>   / </Text>
+                    </View>
+                    <View style={{flex: 2}}>
+                        <TextInput style={ styles.numberInput} placeholder="0.0" keyboardType="numeric" value={form.denominador} onChangeText={ (value) => onChanges(value, 'denominador')}/>
+                    </View>
+                </View>
+                <ActionsButtonsComponent calcular={calcular} limpiar={limpiar}/>
+            </View>
+            <View style={{margin: 30}}>
+                <Text style={{textAlign: 'center', fontSize: 20, color: '#37cc70' }}>La fraccion equivale a:</Text>
+                <Text style={{textAlign: 'center', fontSize: 50, color: '#37cc70' }}>% {result}</Text>
+            </View>
         </View>
     )
 }
